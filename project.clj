88599d052b8ffc2c80befd17dda76cdeb301bb6a
Dimitrios Piliouras <jimpil1985@gmail.com>
2023-12-15 21:06:26 +0000

    add release-tasks

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.jimpil/dblocks "0.1.0-SNAPSHOT"
+(defproject com.github.jimpil/dblocks "0.1.2-SNAPSHOT"
   :description "Clojure macros for leveraging PostgreSQL advisory locks"
   :url "http://github.com/jimpil/dblocks"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,4 +6,17 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.github.seancorfield/next.jdbc "1.3.894"]]
   :profiles {:dev {:dependencies [[org.postgresql/postgresql "9.4.1208.jre7"]]}}
-  :repl-options {:init-ns com.github.jimpil.dblocks.core})
+  :repl-options {:init-ns com.github.jimpil.dblocks.core}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" ]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ;["vcs" "push"]
+                  ]
+  :deploy-repositories [["releases" :clojars]] ;; lein release :patch
+  :signing {:gpg-key "jimpil1985@gmail.com"}
+
+  )
