e5cdef1d0f7cd6b9caaa6bf16e0474f56e9ebc88
Dimitrios Piliouras <jimpil1985@gmail.com>
2023-12-15 21:07:36 +0000

    fix https :url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.github.jimpil/dblocks "0.1.2-SNAPSHOT"
   :description "Clojure macros for leveraging PostgreSQL advisory locks"
-  :url "http://github.com/jimpil/dblocks"
+  :url "https://github.com/jimpil/dblocks"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
